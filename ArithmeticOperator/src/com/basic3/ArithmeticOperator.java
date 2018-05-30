d3142ea04318491cd14bb1a21b36afe3695db340
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-30 12:21:22 +0530

    arithmetic operator program

---
+++ ArithmeticOperator/src/com/basic3/ArithmeticOperator.java
@@ -0,0 +1,19 @@
+package com.basic3;
+
+public class ArithmeticOperator {
+	public static void main(String args[])
+	{
+		int x=40;
+		int y=20;
+		int result;
+		result=x+y;
+		System.out.println("add="+result);
+		result=x-y;
+		System.out.println("sub="+result);
+		result=x*y;
+		System.out.println("mul="+result);
+		result=x/y;
+		System.out.println("div="+result);
+	}
+
+}
