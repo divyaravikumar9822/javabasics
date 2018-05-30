ec927ae09ca2472d6384823fcf3f8e8a1d9150e2
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-30 12:34:46 +0530

    unary operator program

---
+++ UnaryOperator/src/com/basic4/UnaryOperator.java
@@ -0,0 +1,23 @@
+package com.basic4;
+
+public class UnaryOperator {
+	public static void main(String args[])
+	{
+		int x=40;
+		int y=20;
+		int result;
+		result=+x;
+		System.out.println("+x="+result);
+		result=-y;
+		System.out.println("-y="+result);
+		result=++x;
+		System.out.println("++x="+result);
+		result=--y;
+		System.out.println("--y="+result);
+		boolean ok=false;
+		System.out.println(ok);
+		System.out.println(!ok);
+		
+	}
+
+}
