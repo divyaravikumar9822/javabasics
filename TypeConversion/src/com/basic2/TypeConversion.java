59566421f262e6aa86560a570f1604c77e410bc5
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-30 12:08:48 +0530

    type conversion from int to byte& double to byte

---
+++ TypeConversion/src/com/basic2/TypeConversion.java
@@ -0,0 +1,17 @@
+package com.basic2;
+
+public class TypeConversion {
+	public static void main(String args[])
+	{
+		byte b;
+		int i=123;
+		double d=132.658;
+		b=(byte)i;
+		System.out.println("i="+i+"b="+b);
+		b=(byte)d;
+		System.out.println("d="+d+"b="+b);
+		
+		
+	}
+
+}
