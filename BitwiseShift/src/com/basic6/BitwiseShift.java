d8bdc9db69f50c5e2329f32fc460840e746e069f
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-30 14:39:47 +0530

    bitwise & shift operator program

---
+++ BitwiseShift/src/com/basic6/BitwiseShift.java
@@ -0,0 +1,32 @@
+package com.basic6;
+
+public class BitwiseShift {
+	public static void main(String args[])
+	{
+	 int a = 60;	
+     int b = 13;	
+     int c = 0;
+
+     c = a & b;       
+     System.out.println("a & b = " + c );
+
+     c = a | b;       
+     System.out.println("a | b = " + c );
+
+     c = a ^ b;       
+     System.out.println("a ^ b = " + c );
+
+     c = ~a;          
+     System.out.println("~a = " + c );
+
+     c = a << 2;      
+     System.out.println("a << 2 = " + c );
+
+     c = a >> 2;      
+     System.out.println("a >> 2  = " + c );
+
+     c = a >>> 2;     
+     System.out.println("a >>> 2 = " + c );
+  }
+
+}
