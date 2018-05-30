43c3279e35f9c411eb2b38386ccf9f7b42f01d39
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-30 10:45:38 +0530

    odd even program

---
+++ OddEven/src/com/basic1/OddEven.java
@@ -0,0 +1,12 @@
+package com.basic1;
+
+public class OddEven {
+	public static void main(String args[]) {
+	int a=5;
+	if(a%2==0)
+	System.out.println("Even");
+	else
+		System.out.println("Odd");
+
+}
+}
