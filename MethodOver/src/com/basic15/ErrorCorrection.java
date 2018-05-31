31f2d1620ed9bbad974aa42ab09bdfb3bfe858bd
divyaravikumar9822 <divyaravikumar9822@gmail.com>
2018-05-31 22:19:09 +0530

    correction &output programs

---
+++ MethodOver/src/com/basic15/ErrorCorrection.java
@@ -0,0 +1,30 @@
+package com.basic15;
+
+public class ErrorCorrection {
+	public static void main(String[] args) {
+		// video 11
+		int i=1;
+			while(i<=10)
+			{
+			System.out.println(i);
+			++i;
+			}
+
+for (double k= 0.1; k != 1.0; k += 0.1 )
+System.out.println( k );
+int n = 2;
+switch (n)
+{
+case 1:
+System.out.println( "The number is 1" );
+break;
+case 2:
+System.out.println( "The number is 2" );
+break;
+default:
+System.out.println( "The number is not 1 or 2" );
+break;
+}
+	}
+}
+
